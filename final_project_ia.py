d34f3071b76c9c5a8e9ea746db5691ef54b909ca
importiaasisa <iampieschneegans@falcon.bentley.edu>
2024-05-03 07:26:27 -0400

    final_project_ia.py

--- final_project_ia.py
+++ final_project_ia.py
@@ -1,7 +1,6 @@
 import pip
 import pandas as pd
 import pydeck as pdk
-import seaborn as sns
 import streamlit as st
 
 st.set_page_config(page_title="Starbucks in the Americas", page_icon=":coffee:",
